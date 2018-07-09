b3a26d1da26c383e7af751ad8d81940c7cd8fa21
Zach <zaspringer@gmail.com>
2018-07-08 22:12:25 -0700

    Finished javascript 1 review

---
+++ index.js
@@ -0,0 +1,93 @@
+//This is a function declaration
+function myFunctionDeclaration () {
+  console.log('This is myFunctionDeclaration firing');
+}
+
+//This is a function invokation. It's how we actually use the function and get a return value
+myFunctionDeclaration()
+
+//function expression
+var funcExpression = function() {
+  console.log('I am the func expression')
+}
+
+
+function outerFunc(name) {
+  function innerFunc() {
+    console.log('Hello, ' + name)
+  }
+  return innerFunc;
+}
+
+var outerFuncReference = outerFunc('John')
+var outerFuncReference2 = outerFunc('James')
+
+outerFuncReference()
+outerFuncReference2()
+
+//scope
+var globalScopeVariable = 'I am a string that is in the global scope'
+function scopeChecker() {
+  var scopeCheckerVar = 'I am at the top of the function scope'
+  function innerScopeFunction() {
+    var innerScopeVariable = 'I am a string that is in local scope'
+    function evenMoreInnerScopeFunction() {
+      console.log('Inner scope check', innerScopeVariable, scopeCheckerVar, globalScopeVariable)
+      var innerInnerVariable = 'I am at the bottom of the scope tree'
+    }
+    evenMoreInnerScopeFunction()
+  }
+  innerScopeFunction()
+}
+scopeChecker()
+
+function lexicalExample() {
+  var someVariable = 'Hello'
+  console.log('some variable in lexical', someVariable)
+  function variableChanger() {
+    console.log('some variable in variable changer', someVariable)
+    var someVariable = 'Not hello'
+  }
+  variableChanger()
+}
+lexicalExample()
+
+
+//Let keyword and block scope
+if(true) {
+  let john = 'John'
+}
+console.log('john', john)
+if(true) {
+  var john = 'John'
+}
+console.log('john', john)
+
+//variables
+//var is a keyword.. you use to it to assign labels and values
+//Variables must start with $, _, or a letter not a number
+var num = 1;
+var string = 'Hello'
+var boolean = true;
+var noValue = null;
+var undefinedValue = undefined;
+var notANumber = NaN;
+
+//if statements
+if(true) {
+  console.log('I will always fire no matter what')
+}
+
+if(false) {
+  console.log('I will never fire')
+}
+
+var trueChecker = true;
+
+if(trueChecker) {
+  console.log('I will fire because the variable evaluates to true')
+}
+
+if(!trueChecker) {
+  console.log('Even though true checker is true, the bang checks for the opposite. So this is really checking to see if trueChecker is false.')
+}
